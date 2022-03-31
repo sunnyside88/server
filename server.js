6d03ef4a8b1ecd1365bc527c68f6d0e8c1fe1734
sansan <puisanlim98@gmail.com>
2022-03-31 17:29:16 +0800

    edited package.json

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const fs = require('fs')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const fs = require("fs");
+require("dotenv").config();
 
-const authRoute = require("./routes/auth")
+const authRoute = require("./routes/auth");
 
-const app = express()
+const app = express();
 
-mongoose.connect(process.env.MONGO_URI_DATABASE, {
+mongoose
+  .connect(process.env.MONGO_URI_DATABASE, {
     useNewUrlParser: true,
-})
-    .then(() => console.log("DB CONNECTED"))
-    .catch(err => console.log("DB CONN ERR:", err));
+  })
+  .then(() => console.log("DB CONNECTED"))
+  .catch((err) => console.log("DB CONN ERR:", err));
 
 //middlewares
-app.use(morgan('dev'))
-app.use(express.json({ limit: '2mb' }));
-app.use(cors())
+app.use(morgan("dev"));
+app.use(express.json({ limit: "2mb" }));
+app.use(cors());
 
 fs.readdirSync("./routes").map((r) => {
-    app.use("/api", require("./routes/" + r))
-})
+  app.use("/api", require("./routes/" + r));
+});
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
-
-
+  console.log(`Server is running on port ${port}`);
+});
